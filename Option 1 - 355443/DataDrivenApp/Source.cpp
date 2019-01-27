58fd88c1eab498818a3bdc1c34278f7c6eb9f21c
JacquesJnr <jacquesvisserjnr@gmail.com>
2019-01-27 17:46:24 +0000

    Added free search functionality

--- Option 1 - 355443/DataDrivenApp/Source.cpp
+++ Option 1 - 355443/DataDrivenApp/Source.cpp
@@ -5,24 +5,25 @@ using namespace std;
 
 int main() {
 	string myData;
-	string search;
+	string search = "Donald";
 	ifstream inFile;
 	inFile.open("sampleTweets.csv");
+
+	if (!inFile) {
+		cout << "Unable to open file" << endl;
+		exit(1);
+	}
+	cout << "Enter word to search for: ";
+	cin >> search;
 
 	size_t pos;
 	while (inFile.good()) {
 		getline(inFile, myData);
 		pos = myData.find(search);
 		if (pos != string::npos) {
-			cout << "Found" << endl;
-		}
-		else
-		{
-			cout << "Nothing Found" << endl;
+			cout << myData << endl;
 		}
 	}
-
-
 
 	system("pause");
 	return 0;
