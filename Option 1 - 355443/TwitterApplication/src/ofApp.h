d0db82d6c8e3c71315e1a068430f974326f8e815
JacquesJnr <jacquesvisserjnr@gmail.com>
2019-01-27 15:49:53 +0000

    Setup Filehandling
    Using fstream to access the sampleTweets file

---
+++ Option 1 - 355443/TwitterApplication/src/ofApp.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "ofMain.h"
+#include <fstream>
+#include <iostream>
+#include <string>
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+};
